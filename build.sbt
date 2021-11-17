d2b770909a5797bb335cdd4420451120b1057028
PoSungKim <psungkr94@likelion.org>
2021-11-17 22:47:03 +0900

    akka-http 추가

--- build.sbt
+++ build.sbt
@@ -6,5 +6,12 @@ scalaVersion := "2.12.5"
 
 val sparkVersion = "3.2.0"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % sparkVersion
+libraryDependencies ++= Seq (
+  "org.apache.spark" %% "spark-core" % sparkVersion,
 
+  "com.typesafe.akka" %% "akka-actor-typed" % "2.6.17",
+  "com.typesafe.akka" %% "akka-stream" % "2.6.17",
+  "com.typesafe.akka" %% "akka-http" % "10.2.7",
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.2.7",
+  "ch.megard" %% "akka-http-cors" % "1.1.2"
+)
--- src/main/scala/com/chatbot_spark/main.scala
+++ src/main/scala/com/chatbot_spark/main.scala
@@ -2,6 +2,19 @@ package com.chatbot_spark
 
 import org.apache.spark.SparkContext
 
+// akka specific
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+// akka http specific
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+// spray specific (JSON marshalling)
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+// cors
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
+
 object main extends App {
   println("Hello World");
 
@@ -9,4 +22,62 @@ object main extends App {
   val sourceRDD = sparkContext.textFile("/Users/posungkim/Desktop/Portfolio/Big_Data_Platform/Files/words.txt")
   sourceRDD.take(1).foreach(println)
 
+  final case class User(id: Long, name: String, email: String)
+
+  implicit val actorSystem = ActorSystem(Behaviors.empty, "akka-http")
+  implicit val userMarshaller: spray.json.RootJsonFormat[User] = jsonFormat3(User.apply)
+
+  val getUser = get {
+    concat(
+      path("hello") {
+        complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, "Hello world from scala akka http server!"))
+      },
+      path("user" / LongNumber) {
+        userid => {
+          println("get user by id : " + userid)
+          userid match {
+            case 1 => complete(User(userid, "Testuser", "test@test.com"))
+            case _ => complete(StatusCodes.NotFound)
+          }
+        }
+      }
+    )
+  }
+
+  val createUser = post {
+    path("user") {
+      entity(as[User]) {
+        user => {
+          println("save user")
+          complete(User(user.id, "Testuser", "test@test.com"))
+        }
+      }
+    }
+  }
+
+  val updateUser = put {
+    path("user") {
+      entity(as[User]) {
+        user => {
+          println("update user")
+          complete(User(user.id, "Testuser", "test@test.com"))
+        }
+      }
+    }
+  }
+
+  val deleteUser = delete {
+    path("user" / LongNumber) {
+      userid => {
+        println(s"user ${userid}")
+        complete(User(userid, "Testuser", "test@test.com"))
+      }
+    }
+  }
+
+  val route = cors() {
+    concat(getUser, createUser, updateUser, deleteUser)
+  }
+
+  val bindFuture = Http().newServerAt("localhost", 8080).bind(route)
 }
